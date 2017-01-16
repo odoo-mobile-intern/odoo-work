0d5bc60bf65f39efd180daebf26a9c926148df79
Vedant Dave <vedantdave21@gmail.com>
2017-01-16 11:39:20 +0530

    [ADD] Added Login activity

--- app/src/main/java/com/odoo/work/LoginActivity.java
+++ app/src/main/java/com/odoo/work/LoginActivity.java
@@ -3,20 +3,35 @@ package com.odoo.work;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.widget.EditText;
 
+import com.odoo.core.rpc.Odoo;
+import com.odoo.core.rpc.handler.OdooVersionException;
+import com.odoo.core.rpc.listeners.IOdooLoginCallback;
+import com.odoo.core.rpc.listeners.OdooConnectionListener;
+import com.odoo.core.rpc.listeners.OdooError;
+import com.odoo.core.support.OUser;
 import com.odoo.work.core.support.OdooActivity;
 
-public class LoginActivity extends OdooActivity implements View.OnClickListener {
 
+public class LoginActivity extends OdooActivity implements View.OnClickListener,
+        IOdooLoginCallback {
+    private EditText editEmail, editPassword;
+    private Odoo odoo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_login_activity);
-
         // click listeners
+        editEmail = (EditText) findViewById(R.id.edit_email);
+        editPassword = (EditText) findViewById(R.id.edit_password);
+
         findViewById(R.id.btnSignup).setOnClickListener(this);
+        findViewById(R.id.btnSignin).setOnClickListener(this);
     }
 
     @Override
@@ -25,6 +40,52 @@ public class LoginActivity extends OdooActivity implements View.OnClickListener 
             case R.id.btnSignup:
                 startActivity(new Intent(this, SignUpActivity.class));
                 break;
+            case R.id.btnSignin:
+                if (isValid()) {
+                    try {
+                        odoo = Odoo.createInstance(this, "http://192.168.199.101:8069");
+                        odoo.setOnConnect(new OdooConnectionListener() {
+                            @Override
+                            public void onConnect(Odoo odoo) {
+                                odoo.authenticate(editEmail.toString(), editPassword.toString(), "odoo-work", LoginActivity.this);
+                            }
+                        });
+                    } catch (OdooVersionException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                }
         }
+    }
+
+    private boolean isValid() {
+
+        editEmail.setError(null);
+        if (editEmail.getText().toString().trim().isEmpty()) {
+            editEmail.setError(getString(R.string.error_enter_email));
+            editEmail.setFocusable(true);
+            return false;
+        }
+
+        editPassword.setError(null);
+        if (editPassword.getText().toString().trim().isEmpty()) {
+            editPassword.setError(getString(R.string.error_enter_password));
+            editPassword.setFocusable(true);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onLoginSuccess(Odoo odoo, OUser user) {
+        //TODO: Create account.
+    }
+
+    @Override
+    public void onLoginFail(OdooError error) {
+        Snackbar.make(getContentView(), R.string.error_login_failed, BaseTransientBottomBar.LENGTH_LONG)
+                .show();
     }
 }
--- app/src/main/java/com/odoo/work/core/support/OdooActivity.java
+++ app/src/main/java/com/odoo/work/core/support/OdooActivity.java
@@ -1,8 +1,12 @@
 package com.odoo.work.core.support;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class OdooActivity extends AppCompatActivity {
 
+    public View getContentView() {
+        return findViewById(android.R.id.content);
+    }
 
 }
