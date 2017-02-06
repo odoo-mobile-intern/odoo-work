4afd85471ee63ec1a894138923ec2d32b5ffde93
asa-odoo <asa@odoo.com>
2017-02-06 12:02:54 +0530

    [ADD] ProjectTeam and ProjectTeamMember modul added

--- app/src/main/java/com/odoo/work/orm/models/ProjectTeamMember.java
+++ app/src/main/java/com/odoo/work/orm/models/ProjectTeamMember.java
@@ -15,35 +15,25 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http:www.gnu.org/licenses/>
  * <p>
- * Created on 24/1/17 11:53 AM
+ * Created on 6/2/17 11:58 AM
  */
 package com.odoo.work.orm.models;
 
 import android.content.Context;
 
-import com.odoo.work.R;
 import com.odoo.work.orm.ColumnType;
 import com.odoo.work.orm.OColumn;
 import com.odoo.work.orm.OModel;
 
-public class ProjectProject extends OModel {
-    public static final String TAG = ProjectProject.class.getSimpleName();
-    private Context mContext;
+public class ProjectTeamMember extends OModel {
+    public static final String TAG = ProjectTeamMember.class.getSimpleName();
 
-    OColumn name = new OColumn("Project Title", ColumnType.VARCHAR);
-    OColumn use_tasks = new OColumn("is Task", ColumnType.BOOLEAN);
-    OColumn label_tasks = new OColumn("Task Label", ColumnType.VARCHAR);
+    OColumn name = new OColumn("Name", ColumnType.VARCHAR);
     OColumn user_id = new OColumn("User Id", ColumnType.MANY2ONE, "res.users");
-    OColumn partner_id = new OColumn("Partner Id", ColumnType.MANY2ONE, "res.partner");
-    //OColumn team_ids = new OColumn("Team id", ColumnType.MANY2MANY, "project.teams");
+    OColumn state = new OColumn("State", ColumnType.VARCHAR);  //new - accepted - rejected
+    OColumn team_id = new OColumn("Team Id", ColumnType.MANY2ONE, "project.teams");
 
-    public ProjectProject(Context context) {
-        super(context, "project.project");
-        mContext = context;
-    }
-
-    @Override
-    public String getAuthority() {
-        return mContext.getString(R.string.project_authority);
+    public ProjectTeamMember(Context context) {
+        super(context, "project.team.members");
     }
 }
--- app/src/main/java/com/odoo/work/orm/models/ProjectTeams.java
+++ app/src/main/java/com/odoo/work/orm/models/ProjectTeams.java
@@ -15,35 +15,23 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http:www.gnu.org/licenses/>
  * <p>
- * Created on 24/1/17 11:53 AM
+ * Created on 6/2/17 11:59 AM
  */
 package com.odoo.work.orm.models;
 
 import android.content.Context;
 
-import com.odoo.work.R;
 import com.odoo.work.orm.ColumnType;
 import com.odoo.work.orm.OColumn;
 import com.odoo.work.orm.OModel;
 
-public class ProjectProject extends OModel {
-    public static final String TAG = ProjectProject.class.getSimpleName();
-    private Context mContext;
+public class ProjectTeams extends OModel {
+    public static final String TAG = ProjectTeams.class.getSimpleName();
 
-    OColumn name = new OColumn("Project Title", ColumnType.VARCHAR);
-    OColumn use_tasks = new OColumn("is Task", ColumnType.BOOLEAN);
-    OColumn label_tasks = new OColumn("Task Label", ColumnType.VARCHAR);
-    OColumn user_id = new OColumn("User Id", ColumnType.MANY2ONE, "res.users");
-    OColumn partner_id = new OColumn("Partner Id", ColumnType.MANY2ONE, "res.partner");
-    //OColumn team_ids = new OColumn("Team id", ColumnType.MANY2MANY, "project.teams");
+    OColumn name = new OColumn("Team Name", ColumnType.VARCHAR);
+    //OColumn team_member_ids = new OColumn("Team member Ids", ColumnType.MANY2MANY, "project.team.members");
 
-    public ProjectProject(Context context) {
-        super(context, "project.project");
-        mContext = context;
-    }
-
-    @Override
-    public String getAuthority() {
-        return mContext.getString(R.string.project_authority);
+    public ProjectTeams(Context context) {
+        super(context, "project.teams");
     }
 }
